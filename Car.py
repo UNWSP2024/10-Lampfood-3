34651370bccd7dd79583bb1ca429d6bc1d95f470
Elliott Morris <131560931+Lampfood@users.noreply.github.com>
2026-04-09 21:58:25 -0500

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,14 +1,32 @@
-# Program # 2: Car Class
-# Write a class named Car that has the following data attributes:
+#Elliott Morris, 4/9/2026, Car.py
 
-# __year_model (for the car's year model)
-# __make (for the make of the car)
-# __speed (for the car's current speed)
-# The Car class should have an __init__ method that accepts the car's year model and make as arguments.  These values should be assigned to the object's __year_model and __make data attributes.  It should also assign 0 to the __speed data attribute.
+class Car:
+    def __init__(self, make, year):
+        self.make = make
+        self.year = year
+        self.speed = 0
 
-# The class should also have the following methods:
+    def accelerate(self):
+        self.speed += 5
 
-# The accelerate method should add 5 to the speed data attribute each time it it called.
-# The brake method should subtract 5 from the speed data attribute each time it is called.
-# The get_speed method should return the current speed.
-# Next, design a program that creates a Car object then calls the accelerate method five times.  After each call to the accelerate method, get the current speed of the car and display it.  The call the brake method.  After each call to the brake method, get the current speed of the car and display it.
+    def brake(self):
+        self.speed -= 5
+
+    def get_speed(self):
+        return self.speed
+
+def main():
+    my_car = Car("Kia Sportage", 2010)
+
+    for i in range(5):
+        my_car.accelerate()
+        speed = my_car.get_speed()
+        print(speed)
+
+    for i in range(5):
+        my_car.brake()
+        speed = my_car.get_speed()
+        print(speed)
+
+if __name__ == '__main__':
+    main()
